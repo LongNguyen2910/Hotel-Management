a66b4471e27a472498d91759440c9a0813153953
Long Nguyen <ndlong2910@gmail.com>
2025-11-03 13:47:12 +0700

    update

--- Hotel-Management/Models/AppDbContext.cs
+++ Hotel-Management/Models/AppDbContext.cs
@@ -63,7 +63,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseOracle("User Id=quanly;Password=quanly123;Data Source=//192.168.208.131:1521/orcl;");
+        => optionsBuilder.UseOracle("Name=Oracle");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
